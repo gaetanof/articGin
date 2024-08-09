be0f3f71187905ce66b314b1205e24bb6f2159d8
gaetanof <franciscogaetano1@gmail.com>
2024-08-09 14:14:08 -0300

    add cart basis

--- frontend/src/Components/Shop.jsx
+++ frontend/src/Components/Shop.jsx
@@ -1,21 +1,65 @@
-// src/components/ShopComponent.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Shop.css';
+import Menu from './Menu';
+import back from '../assets/shop/back.png';
+import batch from '../assets/shop/batch.png';
+import less from '../assets/shop/less.png';
+import more from '../assets/shop/more.png';
+import addToCart from '../assets/shop/addToCart.png';
+import productDescript from '../assets/shop/productDescript.png';
+import cartFilled from '../assets/shop/cartFilled.png';
+import cartNotFilled from '../assets/shop/cartNotFIlled.png';
+import bottle from '../assets/shop/bottle.png';
+import h1Logo from '../assets/logos/h1Logo.png';
 
 const ShopComponent = () => {
+  const [quantity, setQuantity] = useState(1);
+  const [isCartFilled, setIsCartFilled] = useState(false);
+  const unitPrice = 16000;
+
+  const handleIncrease = () => setQuantity(quantity + 1);
+  const handleDecrease = () => quantity > 1 && setQuantity(quantity - 1);
+  const handleAddToCart = () => setIsCartFilled(true);
+
   return (
     <div className="shop-container">
-      <div className="shop-sub-container"></div>
+      <Menu className="shop-menu-button" />
+      <div className="shop-sub-container">
+        <div className="shop-header">
+          <img src={h1Logo} alt="Artic Gin Logo" className="shop-logo" />
+        </div>
+        <div className="shop-main">
+          <img src={back} alt="Back" className="shop-back-button" />
+          <img src={bottle} alt="Bottle" className="shop-bottle" />
+          <img src={batch} alt="Batch Number" className="shop-batch-number" />
+          <img
+            src={productDescript}
+            alt="Product Description"
+            className="shop-product-description"
+          />
+          <div className="shop-price">
+            <p>${unitPrice * quantity} ARS</p>
+          </div>
+          <div className="shop-quantity-control">
+            <img src={less} alt="Decrease Quantity" onClick={handleDecrease} />
+            <p>{quantity}</p>
+            <img src={more} alt="Increase Quantity" onClick={handleIncrease} />
+          </div>
+          <img
+            src={addToCart}
+            alt="Add to Cart"
+            className="shop-add-to-cart"
+            onClick={handleAddToCart}
+          />
+          <img
+            src={isCartFilled ? cartFilled : cartNotFilled}
+            alt="Cart"
+            className="shop-cart-icon"
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default ShopComponent;
-
-//agregar esto si queremos que se muestre el menu
-
-// import Menu from './Menu';
-
-// {/* <main className="shop-main">
-//     <Menu className="shop-menu-button" />
-//   </main> */}
